bbacc1ed548ebc06a0fe743a2222d125e9954083
nicoNaN <nick.hax@gmail.com>
2015-04-16 20:20:56 +0000

    add digitsum

--- digitsum.rb
+++ digitsum.rb
@@ -1,11 +1,21 @@
-def sumdig_r
+def sumdig_r(number)
   
-  
+  return number == 0 ? 0 : number % 10 + sumdig_r( number / 10 )
   
 end
 
-def sumdig_i
+def sumdig_i(number)
   
+  return 0 if number == 0
   
+  number = number.to_s.split("")
+
+  sum = 0
+  
+  number.each do |num|
+    sum += num.to_i
+  end
+  
+  sum
   
 end
