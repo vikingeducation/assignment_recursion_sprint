e7f354c2b67fa7ee17826dfa837705d0aac08401
Donald Kelsey <dmkelseyii@gmail.com>
2015-07-06 11:32:49 -0500

    Finished iterative palindrome.

--- Palindrome.rb
+++ Palindrome.rb
@@ -3,7 +3,22 @@ def palindrome_r (str)
   return true if str.length<2
 
   str[0]==str[-1] ? palindrome_r(str[1..-2]) : false
-  
+
 end
 
-puts palindrome_r ("rwelkcar")
+# puts palindrome_r ("rwelkcar")
+
+def palindrome_i(str)
+  until (str.length < 2)
+    if str[0]==str[-1]
+      str = str[1..-2]
+    else
+      return false
+    end
+  end
+
+  return true
+end
+
+puts palindrome_i("racecar")
+puts palindrome_i("notracecar")
