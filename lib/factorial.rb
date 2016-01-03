bbcfdb5969b71da3ef5982f392aa3dc15c59a866
Steven Chang <prime_pork@hotmail.com>
2016-01-04 00:11:35 +1000

    all tests passed for factorial_r method

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,5 +1,8 @@
 class Factorial
   def factorial_r(integer)
-    raise ArgumentError, "Argument must be an integer >= 0" if integer < 1 || !(integer.is_a? Integer)
+    raise ArgumentError, "Argument must be an integer >= 0" if integer < 0 || !(integer.is_a? Integer)
+    # I think base case is 1
+    return 1 if integer <= 1
+    total = integer * factorial_r(integer -1)
   end
 end
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -21,10 +21,29 @@ describe Factorial do
     end
 
     # factorial_r(0) == 1
+    it 'factorial_r(0) == 1' do
+      expect(factorial.factorial_r(0)).to eq(1)
+    end
+
     # factorial_r(1) == 1
+    it 'factorial_r(1) == 1' do
+      expect(factorial.factorial_r(1)).to eq(1)
+    end
+
     # factorial_r(2) == 2
+    it 'factorial_r(2) == 2' do
+      expect(factorial.factorial_r(2)).to eq(2)
+    end
+
     # factorial_r(3) == 6
+    it 'factorial_r(3) == 6' do
+      expect(factorial.factorial_r(3)).to eq(6)
+    end
+
     # factorial_r(8) == 40320
+    it 'factorial_r(8) == 40320' do
+      expect(factorial.factorial_r(8)).to eq(40320)
+    end
 
   end
 end
