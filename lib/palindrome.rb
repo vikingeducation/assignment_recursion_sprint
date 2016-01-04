c4b57c969873cfc1bdca5c2dec2e437ca5b3adff
Steven Chang <prime_pork@hotmail.com>
2016-01-05 08:16:33 +1000

    recursive palindrome method has been written

---
+++ lib/palindrome.rb
@@ -0,0 +1,32 @@
+class Palindrome
+
+  def palindrome_r(string)
+    # raise error if it's not a string.
+    raise ArgumentError, "Argument must be a string" if !(string.is_a? String)
+
+    # return true if string length is 0 or 1.
+    return true if string.length == 0 || string.length == 1
+
+    # Downcases all letters and then removes any characters that are not in the alphabet (e.g. spaces and punctuation)
+    string = string.downcase.split("")
+    new_array = []
+    string.each do |letter|
+      if ("a".."z").include? letter
+        new_array << letter
+      end
+    end
+    string = new_array.join("")
+
+    # If it's a two letter word and the first letter and second letter are the same, returns true.
+    # elsif first letter and last letter are the same, second the second letter to the second last letter into the recursive method again.
+    # else false.
+    if string.length == 2 && string[0] == string[-1]
+      true
+    elsif string[0] == string[-1]
+      palindrome_r(string[1..-2])
+    else
+      false
+    end
+
+  end
+end
---
+++ spec/palindrome_spec.rb
@@ -0,0 +1,56 @@
+require 'palindrome'
+
+=begin
+A "Palindrome" is a word or sentence which is the same whether read forwards or backwards, for instance "Rise to vote, sir" or "Able was I ere I saw Elba".
+
+Write a recursive method palindrome_r which returns true if a given string is a palindrome. Hint: strings with zero or 1 letters are always palindromes. Note: Do note use the reverse method... too easy!
+Write an iterative method palindrome_i which does the same iteratively.
+=end
+
+describe Palindrome do
+  let(:palindrome){Palindrome.new}
+
+  it 'is an instance of Palindrome' do
+    expect(subject).to be_a(Palindrome)
+  end
+
+  describe '#palindrome_r' do
+    it 'raises an error if argument is not a string' do
+      expect{palindrome.palindrome_r(123)}.to raise_error(ArgumentError)
+    end
+
+    it 'does not raise an error if argument is a string' do
+      expect{palindrome.palindrome_r("")}.not_to raise_error
+    end
+
+    it 'returns true if argument is ""' do
+      expect(palindrome.palindrome_r("")).to eq(true)
+    end
+
+    it 'returns false if argument is "ab"' do
+      expect(palindrome.palindrome_r("ab")).to eq(false)
+    end
+
+    it 'returns true if argument is "bob"' do
+      expect(palindrome.palindrome_r("bob")).to eq(true)
+    end
+
+    it 'returns false if argument is "steven"' do
+      expect(palindrome.palindrome_r("steven")).to eq(false)
+    end
+
+    it 'returns true if argument is "bb"' do
+      expect(palindrome.palindrome_r("bb")).to eq(true)
+    end
+
+    it "returns true if argument is 'Able was I ere I saw Elba'" do
+      expect(palindrome.palindrome_r("Able was I ere I saw elba")).to eq(true)
+    end
+
+    # "Rise to vote, sir" == true
+    it "returns true if argument is 'Rise to vote, sir'" do
+      expect(palindrome.palindrome_r("Rise to vote, sir")).to eq(true)
+    end
+
+  end
+end
