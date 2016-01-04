87f0ec08643c040fd715a730f6b7412d6285ac75
Steven Chang <prime_pork@hotmail.com>
2016-01-05 08:30:17 +1000

    all tests passed for #palindrome_i

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -27,6 +27,33 @@ class Palindrome
     else
       false
     end
+  end
 
+  def palindrome_i(string)
+    # raise error if it's not a string.
+    raise ArgumentError, "Argument must be a string" if !(string.is_a? String)
+
+    # return true if string is 0 or 1 in length.
+    return true if string.length == 0 || string.length == 1
+
+    # Downcases all letters and then removes any characters that are not in the alphabet (e.g. spaces and punctuation)
+    string = string.downcase.split("")
+    new_array = []
+    string.each do |letter|
+      if ("a".."z").include? letter
+        new_array << letter
+      end
+    end
+    string = new_array.join("")
+
+    # returns false if first and last character are different.
+    index = 0
+    while index < string.length do
+      if string[index] != string[index * -1 - 1]
+        return false
+      end
+      index += 1
+    end
+    true
   end
 end
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -53,4 +53,50 @@ describe Palindrome do
     end
 
   end
+
+
+  describe '#palindrome_i' do
+    it 'raises an error if argument is not a string' do
+      expect{palindrome.palindrome_i(123)}.to raise_error(ArgumentError)
+    end
+
+    it 'does not raise an error if argument is a string' do
+      expect{palindrome.palindrome_i("")}.not_to raise_error
+    end
+
+
+    it 'returns true if string.length is 0 of 1' do
+      expect(palindrome.palindrome_i("")).to eq(true)
+    end
+
+    it 'returns true if string.length is 0 of 1' do
+      expect(palindrome.palindrome_i("a")).to eq(true)
+    end
+
+    it 'returns false if argument is "ab"' do
+      expect(palindrome.palindrome_i("ab")).to eq(false)
+    end
+
+    it 'returns true if argument is "bob"' do
+      expect(palindrome.palindrome_i("bob")).to eq(true)
+    end
+
+    it 'returns false if argument is "steven"' do
+      expect(palindrome.palindrome_i("steven")).to eq(false)
+    end
+
+    it 'returns true if argument is "bb"' do
+      expect(palindrome.palindrome_i("bb")).to eq(true)
+    end
+
+    it "returns true if argument is 'Able was I ere I saw Elba'" do
+      expect(palindrome.palindrome_i("Able was I ere I saw elba")).to eq(true)
+    end
+
+    # "Rise to vote, sir" == true
+    it "returns true if argument is 'Rise to vote, sir'" do
+      expect(palindrome.palindrome_i("Rise to vote, sir")).to eq(true)
+    end
+  end
+
 end
