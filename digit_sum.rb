00b35a09dedf7900eca9a848d5770f1a3729dcab
Andrew Baik <and.baik@gmail.com>
2016-01-11 12:11:30 -0500

    finished iterative digit sum method

--- digit_sum.rb
+++ digit_sum.rb
@@ -4,4 +4,15 @@ def sumdig_r(number)
   return (number % 10) + sumdig_r(number / 10)
 end
 
-puts "#{sumdig_r(103)} #{sumdig_r(999)} #{sumdig_r(10)}"
+#iterative sumdig
+def sumdig_i(number)
+  sum = 0
+  until number.to_s.length == 1 && number == 0
+    sum += number % 10
+    number = number / 10
+  end
+  return sum
+end
+
+#puts "#{sumdig_r(103)} #{sumdig_r(999)} #{sumdig_r(8)}"
+#puts "#{sumdig_i(103)} #{sumdig_i(999)} #{sumdig_i(8)}"
