a5cbed5ea3f3a14f582297394f17cac46d17f8d2
Thomas Lo <thomasjinlo@gmail.com>
2016-01-11 09:35:51 -0800

    Finished sum_digits

--- sum_digits.rb
+++ sum_digits.rb
@@ -1,12 +1,14 @@
 module SumDigits
   def sum_dig_r(n)
     return n if n < 10
+    sum_dig_r(n/10) + sum_dig_r(n%10)
   end
 
-  def sum_dig_i
+  def sum_dig_i(n)
     n_array = n.to_s.split("")
     sum = 0
-
-
+    n_array.each { |num| sum += num.to_i }
+    sum
   end
+
 end
