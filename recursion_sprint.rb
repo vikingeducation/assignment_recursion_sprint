d20f7db4e68f4396b5c2fc739e01daba0f47a72b
Kit Langton <kitlangton@gmail.com>
2016-01-11 12:30:41 -0500

    finish recursion sprint

--- recursion_sprint.rb
+++ recursion_sprint.rb
@@ -1,75 +1,35 @@
-
-
 def factorial_r(number)
-
   return number if number <= 1
   number * factorial_r(number - 1)
-
-end 
+end
 
 def factorial_i(number)
-
   (1..number).to_a.inject do |prod,term|
     prod *= term
-  end 
-
-end  
+  end
+end
 
 def sumdig_r(number)
-
-  if number < 10
-     number
-  else    
-    (number % 10) + sumdig_r(number / 10)
-  end  
-
-end 
+  number < 10 ? number : (number % 10) + sumdig_r(number / 10)
+end
 
 def sumdig_i(number)
-
   number.to_s.chars.inject(0) do |sum,term|
     sum += term.to_i
-  end 
-  
-end  
+  end
+end
 
 def palindrome_r(str)
-
-  return true if str.length <= 1 
-
+  return true if str.length <= 1
   return false if str[0] != str[-1]
-  
-  palindrome_r(str[1..-2])
 
-end 
+  palindrome_r(str[1..-2])
+end
 
 def palindrome_i(str)
+  (str.length / 2).times do |index|
+    return false if str[index] != str[-index - 1]
+  end
 
-  return true if str.length <= 1 
-
-  return false if str[0] != str[-1]
-  
-  palindrome_r(str[1..-2])
-
-end 
-
-#puts factorial_r(1)
-#puts factorial_r(5)
-#puts factorial_i(1)
-#puts factorial_i(5)
-
-# puts sumdig_r(1)
-# puts sumdig_r(305)
-# puts sumdig_r(3050)
-# puts sumdig_r(30509)
-
-# puts sumdig_i(1)
-# puts sumdig_i(305)
-# puts sumdig_i(3050)
-# puts sumdig_i(30509)
-
-puts palindrome_r("ABAABA")
-puts palindrome_r("ABAEBA")
-puts palindrome_r("A")
-puts palindrome_r("ABABA")
-puts palindrome_r("ABAEA")
+  true
+end
