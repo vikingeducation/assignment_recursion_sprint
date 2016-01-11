ab5f39793624a71b348265f44e58d8ab764101c8
Andrew Baik <and.baik@gmail.com>
2016-01-11 12:01:54 -0500

    added iterative factorial method

--- factorial.rb
+++ factorial.rb
@@ -4,4 +4,14 @@ def factorial_r(number)
   return number * factorial_r( number - 1 )
 end
 
-#puts "#{factorial_r(3)} #{factorial_r(4)}  #{factorial_r(5)}"
+#iterative factorial
+def factorial_i(number)
+  result = 1
+  (number).downto(1).each do |factor|
+    result *= factor
+  end
+  result
+end
+
+#puts "#{factorial_r(3)} #{factorial_r(4)} #{factorial_r(5)}"
+#puts "#{factorial_i(3)} #{factorial_i(4)} #{factorial_i(5)}"
