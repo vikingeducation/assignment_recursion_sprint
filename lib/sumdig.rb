2bbbdf648a67bed6c9504717922574bb0d84e3ae
Steven Chang <prime_pork@hotmail.com>
2016-01-12 11:35:29 +1000

    sumdig code refactored into neater code

--- lib/sumdig.rb
+++ lib/sumdig.rb
@@ -3,21 +3,22 @@ class Sumdig
     raise ArgumentError, "Argument must be an integer" if !(number.is_a? Integer)
     if number == 0
       0
-    elsif number < 1
+    elsif number < 0
       sumdig_r(number * -1)
     else
-      (number % 10) + sumdig_r((number - (number % 10))/10)
+      (number % 10) + sumdig_r(number / 10)
     end
   end
 
   def sumdig_i(number)
     raise ArgumentError, "Argument must be an integer" if !(number.is_a? Integer)
     total = 0
-      if number < 0
-        number *= -1
-      end
-    number.to_s.split('').each do |digit|
-      total += digit.to_i
+    if number < 0
+      number *= -1
+    end
+    while number > 0
+      total += number % 10
+      number /= 10
     end
     total
   end
