faab1bfa29e273fc13ac701660ef0589cf45eb11
Steven Chang <prime_pork@hotmail.com>
2016-01-12 11:19:16 +1000

    palindrome code refactored into neater code

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -3,7 +3,7 @@ class Factorial
     raise ArgumentError, "Argument must be an integer >= 0" if integer < 0 || !(integer.is_a? Integer)
     # I think base case is 1
     return 1 if integer <= 1
-    total = integer * factorial_r(integer -1)
+    integer * factorial_r(integer -1)
   end
 
   def factorial_i(integer)
@@ -15,3 +15,5 @@ class Factorial
     total
   end
 end
+
+puts Factorial.new.factorial_i(10)
--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -5,28 +5,15 @@ class Palindrome
     raise ArgumentError, "Argument must be a string" if !(string.is_a? String)
 
     # return true if string length is 0 or 1.
-    return true if string.length == 0 || string.length == 1
+    return true if string.length < 2
 
     # Downcases all letters and then removes any characters that are not in the alphabet (e.g. spaces and punctuation)
-    string = string.downcase.split("")
-    new_array = []
-    string.each do |letter|
-      if ("a".."z").include? letter
-        new_array << letter
-      end
-    end
-    string = new_array.join("")
+    string = downcase_remove_spaces_and_punctuation(string)
 
     # If it's a two letter word and the first letter and second letter are the same, returns true.
     # elsif first letter and last letter are the same, second the second letter to the second last letter into the recursive method again.
     # else false.
-    if string.length == 2 && string[0] == string[-1]
-      true
-    elsif string[0] == string[-1]
-      palindrome_r(string[1..-2])
-    else
-      false
-    end
+    (string.downcase[0] == string.downcase[-1]) && palindrome_r( string[1..-2] )
   end
 
   def palindrome_i(string)
@@ -34,9 +21,20 @@ class Palindrome
     raise ArgumentError, "Argument must be a string" if !(string.is_a? String)
 
     # return true if string is 0 or 1 in length.
-    return true if string.length == 0 || string.length == 1
+    return true if string.length < 2
 
     # Downcases all letters and then removes any characters that are not in the alphabet (e.g. spaces and punctuation)
+    string = downcase_remove_spaces_and_punctuation(string)
+
+    # returns false if first and last character are different.
+    while string.length >= 2
+      return false if string[0] != string[-1]
+      string = string[1..-2]
+    end
+    true
+  end
+
+  def downcase_remove_spaces_and_punctuation(string)
     string = string.downcase.split("")
     new_array = []
     string.each do |letter|
@@ -45,15 +43,5 @@ class Palindrome
       end
     end
     string = new_array.join("")
-
-    # returns false if first and last character are different.
-    index = 0
-    while index < string.length do
-      if string[index] != string[index * -1 - 1]
-        return false
-      end
-      index += 1
-    end
-    true
   end
 end
