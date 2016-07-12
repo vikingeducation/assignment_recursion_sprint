e6229357e7a3828774d3c7c52e6d1d726cdef5ac
David Jiang <davidjgrey@gmail>
2016-07-12 10:06:09 -0700

    more recursive sumdig

--- recursion_sprint.rb
+++ recursion_sprint.rb
@@ -17,18 +17,31 @@ end
 
 
 def sumdig_r(num)
+ return num if num <10
+ num%10 + sumdig_r(num/10)
+end
+
+=begin
+Other versions:
+def sumdig_r(num)
   return num if num < 10
   chars = num.to_s.chars
-  total = 0
+  first_digit=chars[0]
+  first_digit.to_i + sumdig_r(chars[1..-1].join("").to_i)
+end
+
+def sumdig_r(num)
+return num if num < 10
+chars = num.to_s.chars
+total = 0
   while chars.any?
     total += sumdig_r(chars.pop.to_i)
   end
-  total
-end
-
+total
+=end
 
 def sumdig_i(num)
-  num.to_s.split('').map { |item| item.to_i }.reduce(:+)
+  num.to_s.split('').map(&:to_i).reduce(:+)
 end
 
 
