f0bca05353292b508189bff1cab4f0f44e5a72e1
Adrian Mui <adrian.mui@gmail.com>
2016-07-12 12:45:09 -0400

    github was down, factorial_r, factorial_i

---
+++ sprint_recursion.rb
@@ -0,0 +1,23 @@
+
+
+def factorial_r(n)
+  return 1 if n <= 1
+  n * factorial_r(n - 1)
+end
+
+# puts factorial_r(20)
+
+def factorial_i(n)
+  return 1 if n <= 1
+
+  n.downto(1).reduce(1) do |product, item|
+  product * item
+  end
+
+end
+
+# puts factorial_i(7)
+def sumdig_r(n)
+
+   
+end
