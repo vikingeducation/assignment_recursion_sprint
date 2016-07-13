647c081438b7e12dc883cac2d707352d303a7e18
Alex Lach <alexglach@gmail.com>
2016-07-13 09:26:38 -0400

    added edge cases

--- digit_sum.rb
+++ digit_sum.rb
@@ -7,9 +7,10 @@ end
 
 
 def sumdig_i(num)
+  raise "Argument must be a number" if num.to_i == 0
   digits = num.to_s.chars.map(&:to_i)
   digits.inject(:+)
 end
 
-p sumdig_r(223)
+p sumdig_i(555)
 
--- factorial.rb
+++ factorial.rb
@@ -6,8 +6,10 @@ end
 
 
 def factorial_i(num)
+  raise "Number must be greater than zero" if num < 0
+  return 1 if num == 0
   (1..num).inject(:*)
 end
 
 
-p factorial_i(4)
+p factorial_r(-4)
