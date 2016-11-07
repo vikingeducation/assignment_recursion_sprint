9d0bb1ed8562bf3dcde8cd26401c8390c297d94b
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-07 12:56:43 -0500

    Implement iterative and recursive factorial

--- factorial.rb
+++ factorial.rb
@@ -1,8 +1,15 @@
+
 def factorial_r(num)
   return 1 if num <= 1
   num * factorial_r(num-1)
 end
 
-def factorial_i
-
+def factorial_i(num)
+  return 1 if num <= 1
+  factorial = 1
+  num.downto(2) do |number|
+    factorial *= number
+  end
+  factorial
 end
+
