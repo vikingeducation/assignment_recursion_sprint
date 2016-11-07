e0a6b888295590183de6f205dbec8a862de1a43c
Dexter Ford <dexter.ford.77@gmail.com>
2016-11-07 12:56:01 -0600

    Finished!

--- recursion_practice.rb
+++ recursion_practice.rb
@@ -47,8 +47,16 @@ end
 
 def palindrome_r(string)
   return true if string.length == 1 || string.length == 0
-
+  string = string.strip.scan(/\w+/).join.downcase
+  return false if string[0] != string[-1]
+  palindrome_r(string[1..-2])
 end
 
 def palindrome_i(string)
+  string = string.strip.scan(/\w+/).join.downcase
+  while string.length > 1
+    return false if string[0] != string[-1]
+    string = string[1..-2]
+  end
+  true
 end
