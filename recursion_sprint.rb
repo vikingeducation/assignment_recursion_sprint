638cac39cb69595011d36ec506d07ac16293a8fb
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-07 13:38:25 -0500

    done warmups

--- recursion_sprint.rb
+++ recursion_sprint.rb
@@ -1,9 +1,9 @@
-def factorials(num)
+def factorial_r(num)
   return 1 if num == 1 || num == 0
   num * factorials(num-1)
 end
 
-def non_rec_factorials(num)
+def factorial_i(num)
   return 1 if num == 0
   start = num
   num.downto(1) do |int|
@@ -12,13 +12,41 @@ def non_rec_factorials(num)
   num
 end
 
-def digit_sum(num)
+def sumdig_r(num)
   split = num.to_s.split("")
   split = split.inject(0) {|total, i| total += i.to_i}
   return split if split / 10 == 0
   digit_sum(split)
 end
 
-def non_rec_digit_sum(num)
+def sumdig_i(num)
+  split = num
+  continue = true
+  while  continue == true
+    split = split.to_s.split("")
+    split = split.inject(0) {|total, i| total += i.to_i}
+    continue = false if split / 10 == 0
+  end
+  split
 end
 
+def palindrome_r(str)
+  return true if str.length == 1 || str.length == 0
+  return false if str[0] != str[-1]
+  str = str.split("")
+  str.shift
+  str.pop
+  str = str.join
+  palindrome_r(str)
+end
+
+def palindrome_i(str)
+  while str.length !=1 || str.length == 0
+    return false if str[0] != str[-1]
+    str = str.split("")
+    str.shift
+    str.pop
+    str = str.join
+  end
+  true
+end
