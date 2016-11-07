dfc615656a9dbb9a36f15afdd33a4fbd9c2e9947
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-07 13:17:02 -0500

    Implement iterative and recursive palindrome

---
+++ palindrome.rb
@@ -0,0 +1,21 @@
+
+def palindrome_r(string)
+  raise ArgumentError unless string.is_a?(String)
+  if string.length <= 1
+    true
+  elsif string[0] == string[-1]
+    palindrome_r(string[1..-2])
+  else
+    false
+  end
+end
+
+def palindrom_i(string)
+  raise ArgumentError unless string.is_a?(String)
+  while string.length > 1
+    return false unless string[0] == string[-1]
+    string = string[1..-2]
+  end
+  true
+end
+
