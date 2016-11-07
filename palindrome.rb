177794106ac12b5459638575c2ee204ad994af37
Jonathan Popenuck <john.popenuck@gmail.com>
2016-11-07 10:33:54 -0800

    finished palindrome

--- palindrome.rb
+++ palindrome.rb
@@ -6,21 +6,21 @@ def palindrome_r?(str)
     result = str[0] == str[-1] ? true : false
     result
   else
-    if str[0] == str[-1]    
+    if str[0] == str[-1]
       palindrome_r?(str[1..-2])
     else
       return false
     end
   end
-
 end
 
-def palindrome_i(str)
-
+def palindrome_i?(str)
+  str = str.split ''
+  new_str = []
+  str.each do |letter|
+    new_str.unshift(letter)
+  end
+  str = str.join
+  new_str = new_str.join
+  str == new_str ? true : false
 end
-
-p palindrome_r?("amma")
-p palindrome_r?("dmma")
-p "---"
-p palindrome_r?("ambma")
-p palindrome_r?("dmbma")
